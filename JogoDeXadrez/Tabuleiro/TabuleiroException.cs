4d13e33dc477cfbe9845bd125c33901826850e95
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-02 15:56:15 -0300

    Tratando erros com excecao personalizada

---
+++ JogoDeXadrez/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JogoDeXadrez.tabuleiro
+{
+    class TabuleiroException : Exception     
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}   
+
+
